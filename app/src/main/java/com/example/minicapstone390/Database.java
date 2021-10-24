dbf44a1bbab7211dd34ea236092849879ce2851c
NichHarris <harris.nicholas1998@gmail.com>
2021-10-23 21:31:40 -0400

    Revert "Merge"
    This reverts commit 288083bba2861de9b34e9af623679adf9baa3788.

--- app/src/main/java/com/example/minicapstone390/Database.java
+++ app/src/main/java/com/example/minicapstone390/Database.java
@@ -8,4 +8,13 @@ public class Database {
     FirebaseDatabase db = FirebaseDatabase.getInstance();
     DatabaseReference ref =db.getReference("sensors");
 
+    //        DAOSensor dao = new DAOSensor();
+//        Sensor sensor = new Sensor(2, 3.5, "1998-12-12 11:11", "temp");
+//        String key = dao.add(sensor);
+//        System.out.println(key);
+//
+//        Device device = new Device("3", "Test2", false);
+//        DAODevice daoDevice = new DAODevice();
+//        daoDevice.add(device);
+
 }
--- app/src/main/java/com/example/minicapstone390/MainActivity.java
+++ app/src/main/java/com/example/minicapstone390/MainActivity.java
@@ -1,50 +1,39 @@
 package com.example.minicapstone390;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.content.Intent;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    private View loginView, signupView;
-    protected FirebaseDatabase db;
-    protected DatabaseReference ref;
     protected FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        DAOSensor dao = new DAOSensor();
-//        Sensor sensor = new Sensor(2, 3.5, "1998-12-12 11:11", "temp");
-//        String key = dao.add(sensor);
-//        System.out.println(key);
-//
-//        Device device = new Device("3", "Test2", false);
-//        DAODevice daoDevice = new DAODevice();
-//        daoDevice.add(device);
-
-
-        // get the current user
+        // Get the current user
         user = FirebaseAuth.getInstance().getCurrentUser();
 
-        // condition is user is null on loading screen
+        // If Not Authenticated, Send to Login Page
         if (user == null) {
             openLoginActivity();
+        } else {
+            openHomeActivity();
         }
     }
 
     private void openLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void openHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 }
