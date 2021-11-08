36412a013b6b433593cb584f3661a11025c9b2e8
KhaledMat <khaledmatloub2001@gmail.com>
2021-11-08 00:59:51 -0500

    Arduino code with timestamp and IDs

--- Arduino/gasGuard/WCS.h
+++ Arduino/gasGuard/WCS.h
@@ -36,7 +36,17 @@
 
 #include <Arduino.h>
 #include <SPI.h>
-#include "Config.h"
+
+#ifndef FIREBASE_SSL_CLIENT
+#define FIREBASE_SSL_CLIENT
+
+#if __has_include(<WiFiNINA.h>)
+#include <WiFiNINA.h>
+#elif __has_include(<WiFi101.h>)
+#include <WiFi101.h>
+#endif
+
+#endif
 
 
 /// TCP client errors
