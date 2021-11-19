1cbf7bfd2b9b7bf494d270fb792dd0dda028e272
NichHarris <harris.nicholas1998@gmail.com>
2021-11-18 22:42:49 -0500

    Toast issue

--- app/src/main/java/com/example/minicapstone390/Views/DeviceFragment.java
+++ app/src/main/java/com/example/minicapstone390/Views/DeviceFragment.java
@@ -1,5 +1,6 @@
 package com.example.minicapstone390.Views;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -33,14 +34,20 @@ import java.util.Map;
 public class DeviceFragment extends DialogFragment {
     private static final String TAG = "AddDeviceFragment";
     private static final String DEVICES = DatabaseEnv.USERDEVICES.getEnv();
-    private static final String DEVICENAME = DatabaseEnv.DEVICENAME.getEnv();
-    private static final String DEVICELOC = DatabaseEnv.DEVICELOCATION.getEnv();
-    private static final String DEVICESTATUS = DatabaseEnv.DEVICESTATUS.getEnv();
+
     // Declare variables
     private final Database dB = new Database();
 
     protected Button cancelButton, saveButton;
     protected EditText deviceIdInput;
+
+    public HomeActivity activity;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.activity = (HomeActivity) activity;
+    }
 
     // TODO: Replace with check for device ID in database and add it to user
     @Nullable
@@ -67,47 +74,43 @@ public class DeviceFragment extends DialogFragment {
             if (deviceId.isEmpty()) {
                 Toast.makeText(getActivity().getApplicationContext(), "Must Fill All Input Fields!", Toast.LENGTH_LONG).show();
             } else {
-                //TODO: Not sure if this should be done here, probs should only be done on arduino side
-                // We can then check if a device with the ID exists and add, if it doesn't we send an error
-                Device device = new Device(deviceId, deviceId, "Montreal, QC", true);
-                Map<String, Object> deviceAttributes = new HashMap<>();
-                deviceAttributes.put(DEVICENAME, device.getDeviceName());
-                deviceAttributes.put(DEVICELOC, device.getDeviceLocation());
-                deviceAttributes.put(DEVICESTATUS, device.getStatus());
-                dB.getDeviceChild(deviceId).updateChildren(deviceAttributes).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Log.i(TAG, String.format("Added device %s", deviceId));
-                        } else {
-                            Log.e(TAG, String.format("Error adding device %s", deviceId));
-                            Toast.makeText(getActivity() , String.format("Unable to locate device: %s", deviceId), Toast.LENGTH_SHORT).show();
-                            dismiss();
-                        }
-                    }
-                });
 
-                // TODO: Add check if device is already part of user
-                DatabaseReference userRef = dB.getUserChild(dB.getUserId());
-
-                userRef.child(DEVICES).addListenerForSingleValueEvent(new ValueEventListener() {
+                dB.getDeviceChild(deviceId).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (DataSnapshot ds: snapshot.getChildren()) {
-                            if (ds.exists()) {
-                                if (ds.getKey().equals(deviceId)) {
-                                    Log.i(TAG, "User already owns that device");
-                                    Toast.makeText(getActivity() , String.format("User already has device: %s", deviceId), Toast.LENGTH_SHORT).show();
+                        if(snapshot.exists()) {
+                            DatabaseReference userRef = dB.getUserChild(dB.getUserId());
+                            userRef.child(DEVICES).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for (DataSnapshot ds: snapshot.getChildren()) {
+                                        if (ds.exists()) {
+                                            if (ds.getKey().equals(deviceId)) {
+                                                Log.i(TAG, "User already owns that device");
+                                                Toast.makeText(activity, String.format("User already has device: %s", deviceId), Toast.LENGTH_SHORT).show();
+                                                dismiss();
+                                            }
+                                        } else {
+                                            Log.e(TAG, "Unable to locate device");
+                                            dismiss();
+                                        }
+                                    }
+                                    // for updating users with a device
+                                    Map<String, Object> keys = new HashMap<>();
+                                    keys.put(deviceId, deviceId);
+                                    userRef.child(DEVICES).updateChildren(keys);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError e) {
+                                    Log.d(TAG, e.toString());
                                     dismiss();
                                 }
-                            } else {
-                                Log.e(TAG, "Unable to locate device");
-                            }
+                            });
+                        } else {
+                            Log.e(TAG, "No such device ID exists");
+                            Toast.makeText(activity, "No such device ID exists", Toast.LENGTH_SHORT).show();
                         }
-                        // for updating users with a device
-                        Map<String, Object> keys = new HashMap<>();
-                        keys.put(deviceId, deviceId);
-                        userRef.child(DEVICES).updateChildren(keys);
                     }
 
                     @Override
