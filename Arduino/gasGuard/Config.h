1a55ee3c537fe75c3338e25d06983052f2767f05
NichHarris <harris.nicholas1998@gmail.com>
2021-12-01 11:12:27 -0500

    Loading fix

--- Arduino/gasGuard/Config.h
+++ Arduino/gasGuard/Config.h
@@ -19,10 +19,10 @@
 
 
 //in ms
-#define Delay 30000
+#define Delay 5000
 
 // Total time for calibration is CalDelay*CalNum
 // Number of samples used for calibration
-#define CalNum 100
+#define CalNum 6
 // delay between each calibration sample (in ms)
 #define CalDelay 5000 
